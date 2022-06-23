bb7e3ed0740b36a8a77d9f380413c1e96126a91b
Supakit <supakit.pnm@gmail.com>
2022-06-23 16:56:12 +0700

    Add: two way binding

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,8 @@
     <h3>{{ age }}</h3>
     <button @click="setAge">Change Age</button>
     <div>
-      <input type="text" placeholder="First Name" @input="setFirstName" />
-      <input type="text" placeholder="Last Name" @input="setLastName" />
+      <input type="text" placeholder="First Name" v-model="firstName" />
+      <input type="text" placeholder="Last Name" v-model="lastName" />
     </div>
   </section>
 </template>
@@ -30,14 +30,6 @@ export default {
       user.age = 32;
     }
 
-    function setFirstName(e) {
-      firstName.value = e.target.value;
-    }
-
-    function setLastName(e) {
-      lastName.value = e.target.value;
-    }
-
     const userRefs = toRefs(user);
 
     return {
@@ -45,9 +37,9 @@ export default {
       userName: userRefs.name,
       age: userRefs.age,
       setAge: setNewAge,
-      setFirstName,
-      setLastName,
       uName,
+      firstName,
+      lastName,
     };
   },
 };
